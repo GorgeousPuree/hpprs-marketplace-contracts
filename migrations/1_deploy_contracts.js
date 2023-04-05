320072d8ebe85bda6b8c70b270caf0b7de4fb5c1
GorgeousPuree <eydnep2000@gmail.com>
2023-04-05 05:26:22 +0300

    Update.

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,13 +1,13 @@
-const HpprsMarketplaceOrchestrator = artifacts.require("HpprsMarketplaceOrchestrator");
+const HpprsProjectMarketplace = artifacts.require("HpprsProjectMarketplace");
 
 const ERC721Example = artifacts.require("ERC721Example");
 const ERC721AExample = artifacts.require("ERC721AExample");
 const ERC1155Example = artifacts.require("ERC1155Example");
 
 module.exports = async function (deployer) {
-    await deployer.deploy(ERC721Example);
-    await deployer.deploy(ERC721AExample);
-    await deployer.deploy(ERC1155Example);
+    // await deployer.deploy(ERC721Example);
+    // await deployer.deploy(ERC721AExample);
+    // await deployer.deploy(ERC1155Example);
 
-    await deployer.deploy(HpprsMarketplaceOrchestrator);
+    await deployer.deploy(HpprsProjectMarketplace);
 }
